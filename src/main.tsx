b64eeccd7659e11908a39902fff7d007eaec28e8
leevayy <maoaoa7@gmail.com>
2024-07-21 14:22:01 +0300

    hope it will trigger the deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("root not found");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
